57139474b326876f76320ebef0d673685301ad18
alogreer <alogreer@umich.edu>
2024-08-30 23:16:44 -0500

    Added display and up/down/left/right key functionality

--- snake.py
+++ snake.py
@@ -1,7 +1,8 @@
 class Game:
-	def __init__(self, *, wall_symbol="#", ceiling_symbol="#", board_size=10):
+	def __init__(self, *, wall_symbol="#", ceiling_symbol="#", board_size=10, character="@"):
 		self.wall_symbol = wall_symbol
 		self.ceiling_symbol = ceiling_symbol
+		self.character=character
 		self.rows = board_size+2
 		self.cols = board_size+2
 		self.board = [[" " for i in range(self.cols)] for _ in range(self.rows)]
@@ -22,14 +23,41 @@ class Game:
 				print(self.board[row][col], end="")
 			print()
 
-	def play():
-		pass
+	def play(self):
+		# starting position
+		curr_row = self.rows//2
+		curr_col = self.cols//2
+		curr_pos = [curr_row, curr_col]
+		prev_pos = curr_pos
+		self.board[curr_row][curr_col] = self.character
+		curr_dir = "w"
+
+
+
+		while not (curr_pos[0] == 0 or curr_pos[0] == self.rows-1) and not (curr_pos[1] == 0 or curr_pos[1] == self.cols-1):
+			print(curr_pos)
+			self.display()
+			prev_pos = tuple(curr_pos)
+			curr_dir = input()
+			if curr_dir == "w":
+				curr_pos[0] -= 1
+			elif curr_dir == "d":
+				curr_pos[1] += 1
+			elif curr_dir == "s":
+				curr_pos[0] += 1
+			elif curr_dir == "a":
+				curr_pos[1] -= 1
+			self.board[curr_pos[0]][curr_pos[1]] = self.character
+			self.board[prev_pos[0]][prev_pos[1]] = " "
+
+		print("Game Over.")
 
 
 if __name__ == "__main__":
 
 	# game = Game(board_size=10, wall_symbol="|", ceiling_symbol="-")
-	game = Game()
+	game = Game(board_size=20)
 
 	print(game)
-	print(game.display())
+	game.display()
+	game.play()
